69babcc43508ab38e98258c7bfd48d6e7074a54e
thorepet <petersen.thore@gmail.com>
2020-05-19 10:52:02 +0200

    Update documentation

--- R/latex_util.R
+++ R/latex_util.R
@@ -1,5 +1,5 @@
 #' Utiliy function to create Regex pattern.
-#' 
+#'
 #' Utility function to collapse a character vector into a Regex pattern. The
 #' vector elements will be surrounded by word boundaries, and combined with
 #' logical ORs. The whole thing is wrapped in parantheses to catch it as a
@@ -8,7 +8,7 @@
 #' @param x A character vector.
 #' @return The elements of \code{x} surrounded by Regex word boundaries,
 #' collapsed with logical ors, and surrounded by parantheses:
-#' \code{(\\bx[1]\\b|\\bx[2]\\b|...)}.
+#' \code{(\\\\bx[1]\\\\b|\\\\bx[2]\\\\b|...)}.
 .prep_pattern <- function(x) {
   # surround all elements with word boundaries, collapse with logical OR
   pattern <- paste0("\\b", x, "\\b", collapse = "|")
