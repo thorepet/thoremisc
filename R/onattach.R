6b013be873332ed1796dabda958c1bcc2c5f91bd
thorepet <petersen.thore@gmail.com>
2020-05-20 18:54:32 +0200

    Disable .onAttach for now.

--- R/onattach.R
+++ R/onattach.R
@@ -1,19 +1,19 @@
-.onAttach <- function(libname, pkgname) {
-
-  t <- try(
-    expr = {
-      p <- xml2::read_html("https://onelinefun.com/one-liner-of-the-day/")
-      n <- rvest::html_node(p, xpath = "/html/body/div[1]/article/div[1]/p")
-      t <- rvest::html_text(n)
-      packageStartupMessage(paste("Today's joke:", t, sep = "\n"))
-    }
-  )
-
-  if(class(t) %in% "try-error") {
-    packageStartupMessage("No joke today.")
-  }
-
-#   packageStartupMessage(
-#   "Thore's miscellaneous functions ready to work their magic"
+# .onAttach <- function(libname, pkgname) {
+#
+#   t <- try(
+#     expr = {
+#       p <- xml2::read_html("https://onelinefun.com/one-liner-of-the-day/")
+#       n <- rvest::html_node(p, xpath = "/html/body/div[1]/article/div[1]/p")
+#       t <- rvest::html_text(n)
+#       packageStartupMessage(paste("Today's joke:", t, sep = "\n"))
+#     }
 #   )
-}
+#
+#   if(class(t) %in% "try-error") {
+#     packageStartupMessage("No joke today.")
+#   }
+#
+# #   packageStartupMessage(
+# #   "Thore's miscellaneous functions ready to work their magic"
+# #   )
+# }
