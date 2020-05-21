9c30093689288b98642e72d28b1a74629b108b59
thorepet <petersen.thore@gmail.com>
2020-05-21 21:33:03 +0200

    Update documentation.

--- R/latex_acro.R
+++ R/latex_acro.R
@@ -1,11 +1,17 @@
 #' Wrap abbreviations in text in a LaTeX command.
 #'
+#' Wrap abbreviations in a given LaTeX command to toggle the \code{acronym} or
+#' \code{glossary} packages. \code{command} defaults to the \code{acronym}
+#' package's short command, that does not mark the abbreviation as used (the *).
+#' This avoids the acronym being rendered the first time in the list of figures
+#' or tables.
+#'
 #' @param string A character vector.
 #' @param acros A character vector of abbreviations to wrap.
 #' @param command A character (without the preceding backslash).
 #' @return \code{string} with all \code{acros} wrapped in \code{command}.
 #' @examples
-#' latex_acro("This package is not on CRAN.", "CRAN")
+#' latex_acro("This package is not on cran.", "cran")
 #' @export
 latex_acro <- function(string, acros, command = "acs*") {
   pattern <- .prep_pattern(acros)
