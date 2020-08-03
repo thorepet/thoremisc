73a0743ed417ae0e9141488220bfe31d2c019991
thorepet <petersen.thore@gmail.com>
2020-08-03 13:20:39 +0200

    First implementation of string cleaning.

---
+++ R/string_clean.R
@@ -0,0 +1,31 @@
+#' Clean letters in a string.
+#'
+#' Transform \code{string} into a simple lowercase ASCII representation. Options
+#' to convert \code{string} to lowercase, remove non-letter characters, replace
+#' German Umlaute, and transliterate diacritical letters (á, å, ê, ...).
+#'
+#' @param string A character vector.
+#' @param lower Boolean, convert \code{string} to lowercase.
+#' @param spec_chars Boolean, remove non-letters.
+#' @param umlaute Boolean, replace German Umlaute.
+#' @param diacritics Boolean, transliterate diacritical letters.
+#' @return \code{string}, modified.
+#' @examples
+#' string_clean("Thîs sŧriñg will bé å løt simplêr. Köln is not in Schleswig-Holstein.")
+#' @export
+string_clean <- function(
+  string, lower = TRUE, spec_chars = TRUE, umlaute = TRUE, diacritics = TRUE
+) {
+  # The order matters. German Umlaute are diacritics, and are converted into
+  # their "base" letters by remove_diacritics() (ä->a). To preserve them, use
+  # replace_umlaute() first (ä->ae).
+
+  if(lower) string <- tolower(string)
+  if(spec_chars) string <- .remove_special_chars(string)
+  if(umlaute) string <- .replace_umlaute(string)
+  if(diacritics) string <- .remove_diacritics(string)
+
+  string <- string_redund_ws(string)
+
+  return(string)
+}
---
+++ R/string_util.R
@@ -0,0 +1,91 @@
+#' Utility functions for string processing.
+#'
+#' Replace any characters that do not belong to Regex classes \\w or \\d, or are
+#' a literal whitespace, by a single whitespace. The function preserves German
+#' Umlaute and diacritical letters.
+#'
+#' Elaboration on the Regex classes: https://stackoverflow.com/a/2998550/13542638.
+#'
+#' @param string A character vector.
+#' @return \code{string} with special Unicode characters replaced by a whitespace.
+#' @examples
+#' .remove_special_chars("The following will be modified: hello-world.")
+.remove_special_chars <- function(string) {
+  string <- str_replace_all(string, "[^\\w\\d ]", " ")
+  return(string)
+}
+
+#' Utility functions for string processing.
+#'
+#' Replace German Umlaute by their ASCII representations: "ä"->"ae", "ö"->"oe",
+#' and "ü"->"ue". "ß" is diacritical, see \code{.remove_diacritics()}.
+#'
+#' @param string A character vector.
+#' @return \code{string} with German Umlaute replaced.
+#' @examples
+#' .replace_umlaute("Äh, trörö in Überlingen, nicht auf dem Darß.")
+.replace_umlaute <- function(string) {
+  df <- data.frame(
+    umlaut = c("ä", "ö", "ü"),
+    replace = c("ae", "oe", "ue")
+  )
+
+  for(i in 1:nrow(df)) {
+    string <- str_replace_all(string, df$umlaut[i], df$replace[i])
+  }
+
+  return(string)
+}
+
+#' Utility functions for string processing.
+#'
+#' Replace diacritical letters(é, ç, ...) with their "plain" versions. This
+#' function can only handle diacritical letters from latin alphabets. Elements
+#' in \code{string} containinig non-latin letters (e.g. cyrillic), will be
+#' replaced by \code{NA}.
+#'
+#' Reference: https://stackoverflow.com/a/20495866/13542638
+#'
+#' @param string A character vector.
+#' @return \code{string} with diacritical letters replaced by their ASCII
+#'   versions.
+#' @examples
+#' .remove_diacritics("Åll thëşé fūñny leŧters wîll be nørmalised.")
+.remove_diacritics <- function(string) {
+  if(any(str_detect(string, "[äöü]"), na.rm = TRUE)) warning("Umlaute dropped.")
+
+  # transliterate to ASCII: á->'a, è->`e, ë->"e, â->^a, æ->ae, ç->c, å->a, ...
+  string <- iconv(string, to = "ASCII//TRANSLIT")
+
+  # remove "'", "`", "^", and """
+  string <- str_remove_all(string, "['`\\^\"~]")
+
+  if(any(is.na(string))) warning("Some strings could not be transliterated.")
+
+  return(string)
+}
+
+#' Remove redundant whitespaces from a string.
+#'
+#' Replace consecutive whitespaces by a single one, and trim leading and
+#' trailing whitespace.
+#'
+#' @param string A character vector.
+#' @return \code{string} with all \code{acros} wrapped in \code{command}.
+#' @examples
+#' string_redund_ws(" This  string   will be shorter.   ")
+#' @export
+string_redund_ws <- function(string) {
+  # Replace multiple whitespaces by a single one, and trim leading and trailing
+  # whitespaces.
+  #
+  # Args:
+  #   s: character, vector of strings to clean
+  # Returns:
+  #   s, modified
+
+  string <- str_replace_all(string, "\\s+", " ")
+  string <- trimws(string)
+
+  return(s)
+}
